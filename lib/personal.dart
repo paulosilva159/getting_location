b7b22dfac5df0f3e087deb248d3929ff421355a9
Paulo Victor <36122105+paulosilva159@users.noreply.github.com>
2020-06-23 13:15:15 -0400

    Update personal.dart

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,3 +1,3 @@
-String ip = '192.168.0.7';
-String projectId = '5ef0a8c2e46ac';
-String collectionId = '5ef210a39d41a';
+String ip = '[YOUR LOCAL IP]';
+String projectId = '[YOUR PROJECT ID]';
+String collectionId = '[YOUR COLLECTION ID]';
